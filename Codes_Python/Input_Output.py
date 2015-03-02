5ae282e8ad1aec56fef9624fb5e9f3f18a2239e8
semproms-64 <danubio64@outlook.com>
2015-03-02 14:33:54 +0100

    Removing

--- Codes_Python/Input_Output.py
+++
@@ -1,5 +0,0 @@
-myName = input("What is your name?")
-var1 = 1
-var2 = 10
-print(str(var1) + " & " + str(var2))
-print "s" in myName
--- Codes_Python/for_loop.py
+++
@@ -1,4 +0,0 @@
-print("Comienzo")
-for i in [0,1,2]:
-	print(i)
-print("Final")
